019a700bdba646f6c8b55fe53d2f8b5bfea41063
jiwon010106 <jjiwon753@gmail.com>
2024-11-07 09:35:39 +0900

    delete commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { navMenus } from "../utils/data";
-
 import { FcGoogle } from "react-icons/fc";
 
-const Navbar = (menuIdx) => {
+const Navbar = ({ menuIdx }) => {
   // console.log(menuIdx);
   return (
     <nav className="navi bg-[#212121] w-1/5 h-full rounded-sm border border-gray-500 py-10 px-4 flex flex-col justify-between items-center">
       <div className="logo-wrapper flex w-full items-center justify-center gap-8">
         <div className="logo"></div>
         <h2 className="font-semibold text-xl">
-          <Link to="/">Jane</Link>
+          <Link to="/">MARSHALL</Link>
         </h2>
       </div>
 
@@ -20,19 +19,18 @@ const Navbar = (menuIdx) => {
           <li
             key={idx}
             className={`rounded-sm mb-1 border border-gray-700 hover:bg-gray-950 transition-all duration-300 ${
-              menu.idx === menuIdx ? "bg-gray-950 " : ""
+              menu.idx === menuIdx ? "bg-gray-950" : ""
             }`}
           >
-            <Link to={menu.to} className="flex gap4 items-center py-2 px-10">
-              {menu.icon}
-              {menu.label}
+            <Link to={menu.to} className="flex gap-4 items-center py-2 px-10">
+              {menu.icon} {menu.label}
             </Link>
           </li>
         ))}
       </ul>
 
       <div className="w-4/5">
-        <button className="flex justify-center items-center gap-2 bg-gray-300 text-gray-900 py-3 px-4 rounded-sm w-full">
+        <button className="flex justify-center items-center gap-2 bg-gray-300 text-gray-900 py-3 px-4 rounded-md w-full">
           <FcGoogle className="w-5 h-5" />
           <span className="text-sm">Google Login</span>
         </button>
